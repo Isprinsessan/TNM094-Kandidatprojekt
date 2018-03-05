93d09b927694ce9838c4fe85488dd52550b42c4c
Bapiro <lucas.rung@gmail.com>
2018-03-05 09:34:39 +0100

    script

---
+++ Kandidatprojekt/Assets/tracking/Scripts/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using Vuforia;
+using UnityEngine.UI;
+
+public class ViewModel : MonoBehaviour
+{
+    //Initilize camera
+    Camera MainCamera;
+    //private Color mClearColor = new Color(0, 0, 0, 0);
+    public void toggle_changed(bool cameraActive){
+        //Sätter så Maincamera är kameran som används
+        MainCamera = Camera.main;
+        if(cameraActive){
+            //sätter på kamera feed
+            MainCamera.clearFlags = CameraClearFlags.Depth;
+        }
+        else{
+            //sätter skybox
+            MainCamera.clearFlags = CameraClearFlags.Skybox;  
+        }
+       
+    }
+}

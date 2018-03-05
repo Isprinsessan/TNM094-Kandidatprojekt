e9619fbce2b8a7b0ff7858bdecacfa8ef55ea08f
Mikael Lundell <miklun96@gmail.com>
2018-03-05 14:44:56 +0100

    base color same as player

---
+++ Kandidatprojekt/Assets/Scripts/NetworkLobbyHook2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook2 : LobbyHook
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        /*NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
+        spaceship.name = lobby.name;
+        spaceship.color = lobby.playerColor;
+        spaceship.score = 0;
+        spaceship.lifeCount = 3; */
+
+        PlayerController IGNPlayer = gamePlayer.GetComponent<PlayerController>();
+        IGNPlayer.color = lobby.playerColor;
+        IGNPlayer.name = lobby.name;
+        
+        
+    }
+}
